04b944c9e887baf39075d53401d2a323858f7f44
Bruna Rodrigues <brunaaheloisa07@gmail.com>
2024-04-03 20:03:26 -0300

    adicionando alterações nas pastas

--- calculadora.js
+++ calculadora.js
@@ -1,23 +1,31 @@
+
+
 function calcularPagamento() {
     // Recupera os valores dos campos do formulário
-    var valorConta = parseFloat(document.getElementById("valor").value);
-    var numeroPessoas = parseInt(document.getElementById("pessoas").value);
-    var metodo = parseInt(document.getElementById("metodo").value);
+    const valorConta = parseFloat(document.getElementById("valor").value);
+    const numeroPessoas = parseInt(document.getElementById("pessoas").value);
+    const metodo = parseInt(document.getElementById("metodo").value);
+    const gorjeta = document.getElementById("gorjeta").checked ? valorConta * 0.1 : 0;
 
     // Executa a lógica JavaScript original com base nos valores do formulário
-    var pagamentoCliente = valorConta / numeroPessoas;
-    var desconto = (pagamentoCliente / 100) * 10;
+    let pagamentoCliente = valorConta / numeroPessoas;
+
+    if (gorjeta > 0) {
+        pagamentoCliente += gorjeta;
+    }
 
     while(true){
         console.log("Método de pagamento:");
         if(metodo === 1) {
-            var pagamento = pagamentoCliente - desconto;
+            let desconto = (pagamentoCliente / 100) * 10;
+            let pagamento = pagamentoCliente - desconto;
             alert("Adicionado 10% de desconto!!\nO total deste cliente é: R$" + pagamento.toFixed(2));
             break;
         } else if(metodo === 2) { 
-            var pagamento = valorConta / numeroPessoas;
-            alert("O total deste cliente é: R$" + pagamento.toFixed(2));
+            alert("O total deste cliente é: R$" + pagamentoCliente.toFixed(2));
             break;
         }
     }
 }
+
+
